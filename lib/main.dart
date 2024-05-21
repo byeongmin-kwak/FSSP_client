afa1169d9341e861bdb7afc38d88fc85b5a2bb1f
byeongmin-kwak <bqudmals@naver.com>
2024-05-21 21:08:36 +0900

    클라이언트 id 환경변수 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:fullstack_service_programing/widgets/bottom_nav_widget.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await NaverMapSdk.instance.initialize(clientId: '');
+  await dotenv.load(fileName: ".env");
+  String clientId = dotenv.get("CLIENT_ID");
+  await NaverMapSdk.instance.initialize(clientId: clientId);
   runApp(const MyApp());
 }
 
