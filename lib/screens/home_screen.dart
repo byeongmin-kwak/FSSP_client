18b709424f1f9b5dd7288443e40d071d5f61fd14
byeongmin-kwak <bqudmals@naver.com>
2024-05-21 17:32:01 +0900

    홈 화면 레이아웃

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,6 +21,34 @@ class Homescreen extends StatelessWidget {
           ],
         ),
       ),
+      body: const Column(
+        children: [
+          SearchBar(
+            trailing: [Icon(Icons.search)],
+            hintText: "주소를 검색해주세요.",
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "#원룸 #오피스텔 #아파트",
+          ),
+          Text(
+            "최신 후기",
+          ),
+          Row(
+            children: [],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text("#수원시 #영통구"),
+          Text("관심 지역 후기"),
+          Row(
+            children: [],
+          ),
+        ],
+      ),
     );
   }
 }
